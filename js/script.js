90f1f266cce4ba48a19c592542dc51258f2e25a3
saito743 <yuka.saito21@gmail.com>
2020-09-04 22:26:05 +0900

    [Update]

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function(){
+  $('#btn').on('click', function() {
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + "f8b2b59dab0b954a9a255cc7a6ac6cfc",
+      dataType : 'jsonp',
+    }).done(function (data){
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fail(function (data) {
+　　　　alert('通信に失敗しました。');      });
+  });
+});
